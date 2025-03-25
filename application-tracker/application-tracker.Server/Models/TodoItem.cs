e850202b6a29f2169f84114c6aa47fbc338b0fd9
Minardi299 <phu.ngo@outlook.com>
2025-03-25 19:29:45 -0400

    add todo item endpoints

---
+++ application-tracker/application-tracker.Server/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace application_tracker.Server.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ application-tracker/application-tracker.Server/Program.cs
@@ -0,0 +1,35 @@
+using application_tracker.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//TODO add this back when have certificate
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
