82f7246b1d5a4b10235be4541124a1770960fef4
Minardi299 <phu.ngo@outlook.com>
2025-04-05 20:06:46 -0400

    add user object

--- application-tracker/application-tracker.Server/Models/User.cs
+++ application-tracker/application-tracker.Server/Models/User.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Security.Policy;
+
 namespace application_tracker.Server.Models
 {
     public class User
     {
-        public int ID { get; private set; }
-        private string? _username;
-        public string? Username
-        {
-            get { return _username; }
-            set { _username = value; }
-        }
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public ICollection<JobApplication> Applications { get; set; } = new List<JobApplication>();
+
+        public ICollection<ApplicationFolder> Folders { get; set; } = new List<ApplicationFolder>();
     }
 }
