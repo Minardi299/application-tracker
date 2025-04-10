e89fb7818cca030599317029869ac6c68446f1b4
Minardi299 <phu.ngo@outlook.com>
2025-04-10 12:15:08 -0400

    add breadcrumb

--- application-tracker/application-tracker.client/src/components/layout.jsx
+++ application-tracker/application-tracker.client/src/components/layout.jsx
@@ -1,5 +1,8 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar" //
 import { AppSidebar } from "@/components/app-sidebar" //
+import { Separator } from "@/components/ui/separator" //
+import {Breadcrumb,BreadcrumbPage,BreadcrumbSeparator, BreadcrumbItem, BreadcrumbList, BreadcrumbLink} from "@/components/ui/breadcrumb"
+
 import { Outlet } from "react-router-dom"; // Import Outlet
 
 export  function Layout() { // Removed children prop
@@ -7,8 +10,24 @@ export  function Layout() { // Removed children prop
     <SidebarProvider>
       <AppSidebar /> 
       <div className="flex flex-col flex-grow"> 
-        <header >
-           <SidebarTrigger /> 
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
         </header>
         <main > 
           <Outlet /> 
