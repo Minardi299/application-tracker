ed0b4a5d06109c986d888702bf68a1f7ab4361a5
Minardi299 <phu.ngo@outlook.com>
2025-05-05 00:15:57 -0400

    update code with new google sso client

--- application-tracker/application-tracker.client/src/components/login-form.jsx
+++ application-tracker/application-tracker.client/src/components/login-form.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Link } from "react-router-dom";
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin,useGoogleLogin } from '@react-oauth/google';
 import { GoogleLoginButton } from "@/components/GoogleLoginButton"
 "You have created a new client application that uses libraries for user authentication or authorization that are deprecated. New clients must use the new libraries instead. See the [Migration Guide](https://developers.google.com/identity/gsi/web/guides/gis-migration) for more information."
 
@@ -41,6 +41,12 @@ export function LoginForm({
   const handleError = (error) => {
     alert("Error logging in", error);
   };
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => {
+      handleLogin(tokenResponse.id_token); // tokenResponse.access_token or id_token
+    },
+    flow: 'implicit', // or 'auth-code'
+  });
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <form>
@@ -104,7 +110,7 @@ export function LoginForm({
               Continue with Apple
             </Button>
 
-            <Button variant="outline" className="w-full"  type="button" >
+            {/* <Button variant="outline" className="w-full"  type="button"  onClick={() => login()}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path
                   d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -113,7 +119,7 @@ export function LoginForm({
               </svg>
               Continue with Google
             </Button>
-            <GoogleLogin onSuccess={handleLogin} onError={handleError}/>
+            <GoogleLogin onSuccess={handleLogin} onError={handleError}/> */}
             <GoogleLoginButton onSuccess={handleLogin} onFailure={handleError}/>
           </div>
         </div>
