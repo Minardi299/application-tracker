3607c147ca2f1c15ef40eecf31216b758e509ff4
Minardi299 <phu.ngo@outlook.com>
2025-06-26 14:58:15 -0400

    add ismoble to sheet size

--- application-tracker/application-tracker.client/src/components/editing-sheet.jsx
+++ application-tracker/application-tracker.client/src/components/editing-sheet.jsx
@@ -1,13 +1,15 @@
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetDescription } from "@/components/ui/sheet";
 import React from "react";
 import { useGlobalSheet } from "@/context/sheet-provider";
+
+import { useIsMobile } from "@/hooks/use-mobile";
 export function EditingSheet( ){
     const { isOpen, closeSheet, render, title, description } = useGlobalSheet();
+    const isMobile = useIsMobile();
 
-    
     return(
         <Sheet open={isOpen} onOpenChange={closeSheet} modal={false} >
-        <SheetContent className="p-4 overflow-y-auto w-[33vw] min-w-[33vw]">
+        <SheetContent className={`p-4 overflow-y-auto w-[33vw] min-w-[33vw] ${isMobile ? "w-full min-w-full" : ""}`} position="right">
           <SheetHeader>
             <SheetTitle className="truncate leading-normal">{title}</SheetTitle>
             <SheetDescription>{description}</SheetDescription>
