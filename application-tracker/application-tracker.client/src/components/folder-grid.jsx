4eb3abd02b055e449dcd10b1d72e19f587b5f181
Minardi299 <phu.ngo@outlook.com>
2025-06-28 17:14:40 -0400

    add all

--- application-tracker/application-tracker.client/src/components/folder-grid.jsx
+++ application-tracker/application-tracker.client/src/components/folder-grid.jsx
@@ -4,13 +4,11 @@ import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { Folder } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { prefetchApplications } from "@/hooks/use-folder";
+import { prefetchApplications,prefetchAllApplications } from "@/hooks/use-folder";
 import { useQueryClient } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export function FolderGrid() {
-  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { data: folders = [], isLoading } = useFolders();
   const [showAll, setShowAll] = useState(false);
@@ -28,7 +26,7 @@ export function FolderGrid() {
     7: "lg:grid-cols-7",
   };
 
-  const visibleFolders = showAll ? filteredFolders : filteredFolders.slice(0, folderPerRow);
+  const visibleFolders = showAll ? filteredFolders : filteredFolders.slice(0, folderPerRow - 1);
 
   if (isLoading) return <p className="text-muted-foreground">Loading folders...</p>;
   if (!folders.length) return <p className="text-muted-foreground">No folders found.</p>;
@@ -44,7 +42,7 @@ export function FolderGrid() {
             className=" max-w-sm"
           />
         <Button
-            variant="ghost"
+          variant="ghost"
           onClick={() => setShowAll((prev) => !prev)}
           className=" hover:underline"
         >
@@ -56,6 +54,16 @@ export function FolderGrid() {
         "grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4",
         gridCols[folderPerRow]
       )}>
+        <Link
+            to={"/folder/all"}
+            onMouseEnter={() => prefetchAllApplications(queryClient)}
+            className="group flex flex-col items-center justify-center transition-all focus:outline-none focus-visible:ring-2 focus-visible:ring-ring rounded-md"
+          >
+            <p className="text-sm font-medium truncate max-w-[100px] mb-1 text-center">
+              All
+            </p>
+            <Folder className="w-20 h-20 text-primary fill-primary transition-all duration-300 group-hover:-translate-y-1" />
+          </Link>
         {visibleFolders.map((folder) => (
           <Link
             to={`/folder/${folder.id}`}
@@ -70,6 +78,7 @@ export function FolderGrid() {
           </Link>
         ))}
       </div>
+
     </div>
   );
 }
